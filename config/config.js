cd8e8261512f1f3cfcedbaedd7c129d433879488
Valeria Ramos <ramosflorezv@gmail.com>
2022-11-17 18:46:19 -0500

    config arreglos

--- config/config.js
+++ config/config.js
@@ -5,6 +5,10 @@ export default{
     user:process.env.USER ||process.env.DB_USER ,
     password:process.env.PASSWORD ||process.env.DB_PASSWORD ,
     database:process.env.DATABASE ||process.env.DB_DATABASE ,
-    port:process.env.DB_port ||process.env.DB_PORT
+    port:process.env.DB_puerto ||process.env.DB_PORT
 }
-console.log(process.env.DB_HOST)
+console.log("host:",process.env.DB_HOST)
+console.log("user:",process.env.DB_USER)
+console.log("password:",process.env.DB_PASSWORD)
+console.log("database:",process.env.DB_DATABASE)
+console.log("port:",process.env.DB_PORT)
