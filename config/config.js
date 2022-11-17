7a5f1e83e5cf0ffbdb9884106bc037c9f97b2575
Valeria Ramos <ramosflorezv@gmail.com>
2022-11-17 17:09:42 -0500

    pruebas

--- config/config.js
+++ config/config.js
@@ -1,9 +1,10 @@
 import {config} from "dotenv";
 config();// pone a disposición las variables del archivo .env
 export default{
-    host:process.env.HOST ||process.env.DB_HOST ,
+    host:process.env.HOST || process.env.DB_HOST ,
     user:process.env.USER ||process.env.DB_USER ,
     password:process.env.PASSWORD ||process.env.DB_PASSWORD ,
     database:process.env.DATABASE ||process.env.DB_DATABASE ,
     port:process.env.DB_port ||process.env.DB_PORT 
 }
+console.log(process.env.DB_HOST)
--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
-import mysql from "promise-mysql";
+import {createPool} from "mysql2/promise";
 import config from "./config.js";
 
-const connection=mysql.createPool({
+const connection=createPool({
     host:config.host,
     database:config.database,
     user:config.user,
