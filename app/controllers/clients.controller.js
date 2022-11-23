b01c2561b1072d9f651f96345c9be0abf9752c3c
Valeria Ramos <ramosflorezv@gmail.com>
2022-11-23 17:53:43 -0500

    arreglo else

--- app/controllers/clients.controller.js
+++ app/controllers/clients.controller.js
@@ -20,13 +20,15 @@ const addClient= async(req,res)=>{
         const { Client_name,  password, Email }  = req.body;
         if(Client_name===undefined || password===undefined|| Email===undefined){
             res.status(400).json({message:"Bad request. Please fill all field."});
+        }else{
+            const client={ Client_name, role, password, Email } ;
+            const connection= await getConnection();
+     
+            const result=await connection.query("INSERT INTO client_ SET ? ",client);
+            res.json({message:"Client created successfully"});
+    
         }
-        const client={ Client_name, role, password, Email } ;
-        const connection= await getConnection();
- 
-        const result=await connection.query("INSERT INTO client_ SET ? ",client);
-        res.json({message:"Client created successfully"});
-
+        
     }catch(error){
         res.status(500);
         res.send(error.menssage);
